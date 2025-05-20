37a833b0d7a580161c9b0a17b1dc98c778bd27ad
morghybell <moghymorghy@gmail.com>
2025-05-20 10:51:55 +0200

    Diciannovesimo Commit, introduzione tasto logout per erogatore

--- fileJS/Erogatore_Risorse.js
+++ fileJS/Erogatore_Risorse.js
@@ -14,3 +14,21 @@ document.getElementById('decrease-resource').addEventListener('click', () => {
   document.getElementById('user-resource').value = userResources;
   document.getElementById('resource-status').textContent = userResources;
 });
+
+function logout() {
+  showLogoutNotification();
+  setTimeout(() => {
+    window.location.href = 'Home.html';
+  }, 2000); 
+}
+
+function showLogoutNotification() {
+    const message = document.getElementById('logout-message');
+    message.classList.remove('hidden');
+    message.classList.add('show');
+
+    setTimeout(() => {
+        message.classList.remove('show');
+        message.classList.add('hidden');
+    }, 3000);
+}
